7da6e54ebadd38b1b49e042dc34d059dad1eb662
niyikomeyeeric <niyikomeye@gmail.com>
2023-03-20 14:29:49 +0200

    resolved sythax error on line 5

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import  TaskList, TaskDetail 
 
 urlpatterns = [
-    path('', TaskList.as_view(), name='tasks')
-    path('task/<int:pk>/', TaskDetail.as_view(), name='task')
+    path('', TaskList.as_view(), name='tasks'),
+    path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     
 ]
