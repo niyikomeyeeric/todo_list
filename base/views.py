f2c531f0c2a3a327cb91dc1beacde4bda97fad54
niyikomeyeeric <niyikomeye@gmail.com>
2023-03-22 14:32:42 +0200

    added search functionality

--- base/views.py
+++ base/views.py
@@ -43,11 +43,17 @@ class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
 
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__icontains=search_input)
+
+
+            context['search_input'] = search_input
         return context
 
 
